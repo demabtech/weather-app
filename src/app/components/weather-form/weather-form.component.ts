c0876a217fef928261ff79d0f96d45b70d08f536
Carlos Dema <carlosdema@demabtech.com>
2019-11-23 17:05:44 -0500

    Add form functionality

--- src/app/components/weather-form/weather-form.component.ts
+++ src/app/components/weather-form/weather-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-weather-form',
   templateUrl: './weather-form.component.html',
   styleUrls: ['./weather-form.component.css']
 })
-export class WeatherFormComponent implements OnInit {
+export class WeatherFormComponent {
 
+  _city: string;
+  _country: string;
+  @Output() sent = new EventEmitter<any>();
+  
   constructor() { }
 
-  ngOnInit() {
+  onSend() {
+    this.sent.emit({
+      city: this._city,
+      country: this._country
+    });
   }
 
 }
